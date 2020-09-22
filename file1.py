24383e2f53945691dac9db49eab0b7bf326ff16d
anukprakash <36255613+anukprakash@users.noreply.github.com>
2020-09-22 19:34:28 +0530

    Add files via upload

--- file1.py
+++ file1.py
@@ -11,20 +11,3 @@ if ans:
 else:
     print("No")
 
-//this is a comment
-//This is another way
-def isPalindrome(str):
- 
-    # Run loop from 0 to len/2 
-    for i in range(0, int(len(str)/2)): 
-        if str[i] != str[len(str)-i-1]:
-            return False
-    return True
-# main function
-s = "malayalam"
-ans = isPalindrome(s)
- 
-if (ans):
-    print("Yes")
-else:
-   print("No")
